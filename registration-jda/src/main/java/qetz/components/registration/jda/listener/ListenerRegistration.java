0dd0682eb73c954be02eee89f30d6aae1bbd4544
Qetz <general@qetz.de>
2022-05-24 19:46:46 +0200

    Add missing @Inject

--- registration-jda/src/main/java/qetz/components/registration/jda/listener/ListenerRegistration.java
+++ registration-jda/src/main/java/qetz/components/registration/jda/listener/ListenerRegistration.java
@@ -1,5 +1,6 @@
 package qetz.components.registration.jda.listener;
 
+import com.google.inject.Inject;
 import com.google.inject.Injector;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.hooks.EventListener;
@@ -14,6 +15,7 @@ public final class ListenerRegistration {
   private final Injector injector;
   private final JDA jda;
 
+  @Inject
   private ListenerRegistration(
     ComponentScanning components,
     Injector injector,
