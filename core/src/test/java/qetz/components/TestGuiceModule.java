92643ed3fd99a4568a58696d0f7e65215941a691
Qetz <general@qetz.de>
2022-05-26 15:41:45 +0200

    Remove lombok code

--- core/src/test/java/qetz/components/TestGuiceModule.java
+++ core/src/test/java/qetz/components/TestGuiceModule.java
@@ -2,12 +2,11 @@ package qetz.components;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.name.Names;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @Component
 public final class TestGuiceModule extends AbstractModule {
+  private TestGuiceModule() {}
+
   @Override
   protected void configure() {
     bind(String.class).annotatedWith(Names.named("test")).toInstance("test");
