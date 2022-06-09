78290a50bf23e635ff6e27ca30dd2270d03c04ec
Qetz <general@qetz.de>
2022-06-09 19:26:06 +0200

    Add missing static factory method

--- registration-jda/src/main/java/qetz/components/registration/jda/JdaClassScanModule.java
+++ registration-jda/src/main/java/qetz/components/registration/jda/JdaClassScanModule.java
@@ -6,6 +6,12 @@ import qetz.components.ClassScanModule;
 import java.util.Collection;
 
 public final class JdaClassScanModule implements ClassScanModule {
+  public static JdaClassScanModule create() {
+    return new JdaClassScanModule();
+  }
+
+  private JdaClassScanModule() {}
+
   @Override
   public Collection<String> recursivePackages() {
     return Lists.newArrayList(JdaClassScanModule.class.getPackageName());
--- registration-spigot/src/main/java/qetz/components/registration/spigot/SpigotClassScanModule.java
+++ registration-spigot/src/main/java/qetz/components/registration/spigot/SpigotClassScanModule.java
@@ -6,6 +6,12 @@ import qetz.components.ClassScanModule;
 import java.util.Collection;
 
 public final class SpigotClassScanModule implements ClassScanModule {
+  public static SpigotClassScanModule create() {
+    return new SpigotClassScanModule();
+  }
+
+  private SpigotClassScanModule() {}
+
   @Override
   public Collection<String> recursivePackages() {
     return Lists.newArrayList(SpigotClassScanModule.class.getPackageName());
